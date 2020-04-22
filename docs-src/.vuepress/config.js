84bc605e85e74a31a3eb76804721e3e20947ad5b
Gabriel McGoldrick <gabriel.mcgoldrick@hpe.com>
2020-04-22 17:01:37 +0100

    first vssue test

--- docs-src/.vuepress/config.js
+++ docs-src/.vuepress/config.js
@@ -13,6 +13,8 @@ module.exports = {
       repo: 'vsssu-test',
       clientId: '58496616139e25a13d0c',
       clientSecret: '25a6079ae9b60be7c2f6a3a55f6415079e90a263',
+
+      autoCreateIssue: true,
     },
   },  
   themeConfig: {
